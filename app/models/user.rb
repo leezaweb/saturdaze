fe79368bb59b22912b4029a24ef341f8de7ffaf8
azeelleeza <leezaluncheon@gmail.com>
2018-04-18 12:00:46 -0400

    event-planner and stylez

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :rsvps
   has_many :events, foreign_key: :host_id
   has_many :commitments, foreign_key: :guest_id, class_name: 'Rsvp'
+  validates :email, uniqueness: true
+  validates :first_name, :last_name, :email, :password, presence: true
 
   has_secure_password
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,8 @@ end
 
 10.times do |x|
     User.create(first_name:Faker::FamilyGuy.character.split(' ').first,
-    last_name:Faker::FamilyGuy.character.split(' ').last)
+    last_name:Faker::FamilyGuy.character.split(' ').last,
+    password:"password")
     Location.create(address:Faker::Address.street_address, name:Faker::StarWars.planet, description:Faker::Lorem.sentence(4))
     Event.create(name:Faker::SiliconValley.company, location_id:x+1, host_id:x+1, description:Faker::Lorem.sentence(4))
 end
